d4ad535b6c908ea7e5e81edb6996ec81c7984817
Divya chandrapandi <98633621+divyachandrapandi@users.noreply.github.com>
2022-05-31 23:44:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ from form import MovieForm, AddMovie
 # paassowrd="passhatj"
 TMDB_ENDPOINT = "https://api.themoviedb.org/3/search/movie"
 
-TMDB_APIKEY = "e2445d55fc35f6abb011fc7f95372cb7"
+TMDB_APIKEY = "YOUR KEY"
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = 'YOUR KEY'
 Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///moviesnew.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -29,27 +29,11 @@ class Movie(db.Model):
     img_url = db.Column(db.String(2000), nullable=True)
 
 
-# db.create_all()
-# new_movie = Movie(
-#     title="Phone Booth",
-#     year=2002,
-#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's
-#     sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads
-#     to a jaw-dropping climax.",
-#     rating=7.3,
-#     ranking=10,
-#     review="My favourite character was the caller.",
-#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-# )
-# db.session.add(new_movie)
-#
-# db.session.commit()
-
+# db.create_all() COMMENT OUT ONCE TABLE CREATED
 
 @app.route("/")
 def home():
     all_movies = Movie.query.order_by(Movie.rating).all()
-
     # This line loops through all the movies
     for i in range(len(all_movies)):
         # This line gives each movie a new ranking reversed from their order in all_movies
